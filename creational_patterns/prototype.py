a123849471cfda99710c11b327b693463bba0bba
Eucretia Matyala <help.matyalae@ADS.CPUT.AC.ZA>
2025-04-19 14:52:45 +0200

    deleted folders and file

--- creational_patterns/prototype.py
+++
@@ -1,9 +0,0 @@
-import copy
-
-class AssessmentTemplate:
-    def __init__(self, title, instructions):
-        self.title = title
-        self.instructions = instructions
-
-    def clone(self):
-        return copy.deepcopy(self)
